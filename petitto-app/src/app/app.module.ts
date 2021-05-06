5ae5da90068e8b2a313b980e1ddbdd8c8f333488
JP <joao.paulo.b.silva@accenture.com>
2021-05-06 16:02:21 -0300

    creation of menu components

--- petitto-app/src/app/app.module.ts
+++ petitto-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { DoarComponent } from './doar/doar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    LoginComponent,
+    DoarComponent
   ],
   imports: [
     BrowserModule,
