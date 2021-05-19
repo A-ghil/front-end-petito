842cb230a26f9757508e2a5c6e33791cdb80ce90
JP <joao.paulo.b.silva@accenture.com>
2021-05-18 23:08:18 -0300

    Update Navbar

--- petitto-app/src/app/doar/doar.component.ts
+++ petitto-app/src/app/doar/doar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoarComponent implements OnInit {
 
-  titleAdote = 'Adote um PETITTO!'
+  titleAdote = 'Sobre'
 
   constructor() { }
 
