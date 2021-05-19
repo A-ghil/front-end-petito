28e33dc752636c28fad7091764f45417ac0abee5
JP <joao.paulo.b.silva@accenture.com>
2021-05-18 23:35:52 -0300

    Create Forms Component

--- petitto-app/src/app/app.module.ts
+++ petitto-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { LoginComponent } from './login/login.component';
 import { DoarComponent } from './doar/doar.component';
 import { PetsComponent } from './pets/pets.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { NavbarComponent } from './navbar/navbar.component';
     LoginComponent,
     DoarComponent,
     PetsComponent,
-    NavbarComponent
+    NavbarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
